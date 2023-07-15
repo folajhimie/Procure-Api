9eb9d1c9f18a64034665e75054d8b07a3dab58b5
filmo.dev <filmorealty.dev@gmail.com>
2023-07-15 01:38:39 +0100

    working on the server file..

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 
+const cors = require("cors");
 const connectDB = require('./database/connect');
 const PORT = process.env.PORT || 4545;
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to MYSQL application." });
+});
+
 
 const start = async() => {
     try {
